5a39704e767b465f5e460602e42c4c3c2784a0cf
MEHWISH <86172399+mehwishkk@users.noreply.github.com>
2025-01-12 01:56:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pandas
+# dict_comprehension = {new_key:new_value for (index, row) in df.iterrows()}
+
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 phonetic_dict = {row.letter:row.code for(index,row) in data.iterrows() }
 print(phonetic_dict)
